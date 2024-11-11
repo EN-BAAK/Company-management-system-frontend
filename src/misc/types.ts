db77bb6bdd4e41e48e15be825e4d90092239f59d
Bassel Ahmed ABO-Khabsa <baakswaiengineer@gmail.com>
2024-11-11 18:38:25 +0300

    complete project structure init

---
+++ src/misc/types.ts
@@ -0,0 +1,21 @@
+export type ToastMessage = {
+  message: string;
+  type: "SUCCESS" | "ERROR";
+};
+
+export type variant = "info" | "danger" | "warning" | "secondary";
+
+export type Warning = {
+  message: string;
+  btn1: string;
+  btn2: string;
+  styleBtn1?: variant;
+  styleBtn2?: variant;
+  handleBtn2: () => void;
+};
+
+export type AppContext = {
+  isLoggedIn: boolean;
+  showToast: (toastMessage: ToastMessage) => void;
+  showWarning: (warning: Warning) => void;
+};
